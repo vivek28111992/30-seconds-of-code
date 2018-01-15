2ca265ce24e6f311912d801404f145d7fa47149c
Vivek <pawarvivek29@gmail.com>
2018-01-15 23:56:22 +0530

    pullAtValues array

--- Array/arrayPullAtValue.js
+++ Array/arrayPullAtValue.js
@@ -4,8 +4,8 @@
 
 const pullAtValue = (arr, pullArr) => {
 	let removed = []
-		pushToRemove = arr.forEach((v, i) => (pullArr.inlcudes(v) ? removed.push(v) : v)),
-		mutateTo = arr.filter((v, i) =>  !pullArr.inlcudes(v));
+		pushToRemove = arr.forEach((v, i) => (pullArr.includes(v) ? removed.push(v) : v)),
+		mutateTo = arr.filter((v, i) =>  !pullArr.includes(v));
 
 	arr.length = 0;
 	mutateTo.forEach(v => arr.push(v));
